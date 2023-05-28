ce0da6c7cb7da322cf007af61df5c3848860bf54
crimsoncero <crimson.breiman@gmail.com>
2023-05-28 18:50:56 +0300

    1

--- Assets/_Scripts/BallGFXControl.cs
+++ Assets/_Scripts/BallGFXControl.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class BallGFXControl : MonoBehaviour
 {
     [SerializeField] Material _material;
-    [SerializeField] [ColorUsage(true,true)] Color _color;
+    //[SerializeField] [ColorUsage(true,true)] Color _color;
 
     private void Awake()
     {
-        _material.SetColor(Shader.PropertyToID("_Emission"), _color);
+        //_material.SetColor(Shader.PropertyToID("_Emission"), _color);
     }
 
     private void OnValidate()
