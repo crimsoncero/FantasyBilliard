e47707dd6e8a515ef1a68b267b158343b4a14d61
crimsoncero <crimson.breiman@gmail.com>
2023-06-11 17:58:30 +0300

    Ball Appear Disappear
    1

--- Assets/_Scripts/BallHandler.cs
+++ Assets/_Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -5,12 +6,17 @@ public class BallHandler : MonoBehaviour
 {
 
     public bool IsMoving { get; private set; }
-
+    [SerializeField] protected Transform _transform;
     [SerializeField] protected BallData _ballData;
     [SerializeField] protected Rigidbody _rb;
     [SerializeField]
     [Range(0f, 1f)] protected float _dissolve = 0;
+    
+    
+    
     protected Material _material;
+
+
 
     private void Awake()
     {
@@ -35,7 +41,54 @@ public class BallHandler : MonoBehaviour
         }
     }
 
-    private void OnValidate()
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hole"))
+        {
+            Debug.Log("Vaniuh");
+            Vanish();
+        }
+    }
+
+
+    protected void Vanish()
+    {
+        float vanishTime = 2f;
+        float floatHeight = 5f;
+
+        _rb.isKinematic = true;
+        _rb.detectCollisions = false;
+
+        _transform.DOMoveY(transform.position.y + floatHeight, vanishTime).SetEase(Ease.InOutSine);
+        DOVirtual.Float(0, 1, vanishTime,v => _material.SetFloat("_Dissolve", v));
+
+    }
+
+    public void Appear()
+    {
+        float appearTime = 3f;
+        float appearHeight = 5f;
+
+        _rb.isKinematic = true;
+        _rb.detectCollisions = false;
+
+        _transform.position = new Vector3(transform.position.x, transform.position.y + appearHeight, transform.position.z);
+
+
+        _transform.DOMoveY(transform.position.y - appearHeight, appearTime).SetEase(Ease.InOutSine).OnComplete(() => OnAppear());
+        DOVirtual.Float(1, 0, appearTime, v => _material.SetFloat("_Dissolve", v));
+
+
+    }
+
+    protected void OnAppear()
+    {
+        _rb.isKinematic = false;
+        _rb.detectCollisions = true;
+        _transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+    }
+
+private void OnValidate()
     {
         _material = new Material(_ballData.Material);
         _rb.GetComponent<MeshRenderer>().material = _material;
--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -12,31 +12,117 @@ public class GameManager : StaticInstance<GameManager>
     // Enablers
     public bool CanShoot { get; set; }
 
-    // Game Analytics
+    // Game Data
     public List<BallData> BallsInThisGame { get; private set; }
+    public GameState CurrentState { get; private set; }
+    public Player CurrentPlayer { get; private set; }
+    public TurnType CurrentTurnType { get; private set; }
+    public BallType P1BallType { get; private set; }
+    public BallType P2BallType { get; private set; }
 
-
-    // Turn Analytics
+    // Turn Data
     public BallData FirstContact { get; private set; }
     public List<BallData> BallsInThisTurn { get; private set; }
+
+    
 
     private void Start()
     {
         CanShoot = true;
+        CurrentState = GameState.Starting;
+        OnStartingEnter();
     }
  
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(CurrentState == GameState.Player) 
+        {
+            OnPlayerUpdate();
+        }
     }
+
+    void OnStartingEnter()
+    {
+        // Choose First Player to play.
+        CurrentPlayer = Random.Range(0,1) == 0 ? Player.P1 : Player.P2;
+        foreach(var b in Balls)
+        {
+            b.Appear();
+        }
+
+        OnPlayerEnter();
+    }
+
+
+    void OnPlayerEnter()
+    {
+        CurrentState = GameState.Player;
+    }
+
+    void OnPlayerUpdate()
+    {
+
+    }
+
+    void OnPlayerExit()
+    {
+
+        if (false) // 8 Ball in, game ends
+        {
+            bool isWinner = false; // Check if won or lost
+            OnEndingEnter(true);
+        }
+        else if (false) // Cue Ball in, penalty for opponent.
+        {
+            CurrentPlayer = CurrentPlayer == Player.P1 ? Player.P2 : Player.P1;
+            CurrentTurnType = TurnType.PenaltyEx;
+        }
+        else if (false) // First contact was not player's ball.
+        {
+            CurrentPlayer = CurrentPlayer == Player.P1 ? Player.P2 : Player.P1;
+            CurrentTurnType = TurnType.Penalty;
+        }
+        else if (false) // Scored a ball - Another turn.
+        {
+            CurrentTurnType = TurnType.Extra;
+        }
+        else // Nothing special happend
+        {
+            CurrentPlayer = CurrentPlayer == Player.P1 ? Player.P2 : Player.P1;
+            CurrentTurnType = TurnType.Normal;
+        }
+
+        OnPlayerEnter();
+    }
+
+    void OnEndingEnter(bool isWinner)
+    {
+
+    }
+
+
 }
 
-enum GameState
+
+public enum TurnType
+{
+    Normal,
+    Extra,
+    Penalty,
+    PenaltyEx,
+}
+
+public enum Player
+{
+    P1,
+    P2,
+}
+
+public enum GameState
 {
     Starting,
-    PlayerOne,
-    PlayerTwo,
+    Player,
     Ending,
 }
